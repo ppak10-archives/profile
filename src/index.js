af9fa327da6a9d337a2b16207142623e17f6b651
Peter <ppak10@gmail.com>
2020-03-29 15:41:59 -0700

    Adds triangular shape background

--- src/index.js
+++ src/index.js
@@ -6,17 +6,53 @@
 // Styles
 import 'styles/main.less';
 
+// Constants
+const EDGE_LENGTH = 25;
+const COLORS = ['red', 'oragne', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const drawTriangle = (canvas, x, y, orientation) => {
+  let yCoefficient = 1;
+  if (orientation === 'up') {
+    yCoefficient = -1;
+  } else if (orientation === 'down') {
+    yCoefficient = 1;
+  }
+  const ctx = canvas.getContext('2d');
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + EDGE_LENGTH, y);
+  ctx.lineTo(x + EDGE_LENGTH / 2, y + yCoefficient * EDGE_LENGTH);
+  ctx.lineTo(x, y);
+  ctx.closePath();
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+  ctx.stroke();
+  ctx.fillStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+  ctx.fill();
+};
+
 const intialize = () => {
   const canvas = document.getElementById('background');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  const I_MAX = window.innerWidth / EDGE_LENGTH + 1;
+  const J_MAX = window.innerHeight / EDGE_LENGTH + 1;
+
   if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(10, 10, 50, 250);
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 50, 50);
+    for (let j = 0; j < J_MAX; j++) {
+      for (let i = 0; i < I_MAX; i++) {
+        // Triangles for row A
+        const xRowA = i * EDGE_LENGTH - (EDGE_LENGTH / 2) * (j % 2);
+        const yRowA = j * EDGE_LENGTH;
+        drawTriangle(canvas, xRowA, yRowA, 'down');
+
+        // Triangles for row B
+        const xRowB = i * EDGE_LENGTH - EDGE_LENGTH / 2 ** ((j + 1) % 2);
+        const yRowB = (j + 1) * EDGE_LENGTH;
+        drawTriangle(canvas, xRowB, yRowB, 'up');
+      }
+    }
   }
 };
 
