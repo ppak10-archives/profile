78de2b2f9d645ce470f0a32afd1f6fa45a6a08f3
Peter <ppak10@gmail.com>
2020-04-03 19:09:14 -0700

    Adds handling for click events

--- src/canvas.js
+++ src/canvas.js
@@ -5,9 +5,9 @@
 
 // Constants
 import CANVAS from 'constants/canvas.json';
+import TRIANGLE from 'constants/triangle.json';
 
-const COLORS = ['red', 'oragne', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-const EDGE_LENGTH = 50;
+const EDGE_LENGTH = 25;
 
 // Draws extra set of triangles past maximum index values to hide white space
 const RESIZE_BUFFER = 1;
@@ -20,9 +20,11 @@ export default class Canvas {
       CANVAS.marginRight +
       RESIZE_BUFFER;
     this.jMax = window.innerHeight / EDGE_LENGTH - CANVAS.marginBottom;
+    this.i = null;
+    this.j = null;
   }
 
-  draw() {
+  drawTiling() {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.iMax =
@@ -36,22 +38,69 @@ export default class Canvas {
         for (let i = CANVAS.marginLeft; i < this.iMax; i++) {
           const orientation = (j + i) % 2 ? 'down' : 'up';
 
-          // Draw triangle
-          const triangle = new Triangle(i, j, orientation);
-          triangle.draw();
+          if (
+            this.i !== null &&
+            this.j !== null &&
+            this.i === i &&
+            this.j === j
+          ) {
+            // Draw triangle
+            const options = {
+              fillStyle: 'blue',
+            };
+            const triangle = new Triangle(i, j, orientation, options);
+            triangle.draw();
+          } else {
+            // Draw triangle
+            const triangle = new Triangle(i, j, orientation);
+            triangle.draw();
+          }
         }
       }
     }
   }
 
+  onClick(e) {
+    if (!CANVAS.coordinateType) {
+      this.mouseX = e.x;
+      this.mouseY = e.y;
+    } else {
+      this.mouseX = e[`${CANVAS.coordinateType}X`];
+      this.mouseY = e[`${CANVAS.coordinateType}Y`];
+    }
+
+    // Calculates i and j values for triangle tiling
+    this.j = Math.floor(this.mouseY / EDGE_LENGTH);
+    const xDown = (EDGE_LENGTH - (this.mouseY % EDGE_LENGTH)) / 2;
+    const xUp = (this.mouseY % EDGE_LENGTH) / 2;
+    const xFirstOffset = ((this.j + 1) % 2) * xUp + (this.j % 2) * xDown;
+    const xFirstPairTriangle =
+      2 * ((this.j % 2) * xUp + ((this.j + 1) % 2) * xDown);
+    if (this.mouseX - xFirstOffset <= 0) {
+      this.i = 0;
+    } else {
+      const xCompletePairs = Math.floor(
+          (this.mouseX - xFirstOffset) / (2 * (xDown + xUp)),
+      );
+      const xRemainder =
+        this.mouseX - xFirstOffset - xCompletePairs * (2 * (xDown + xUp));
+      const xRemainderTriangles = xRemainder - xFirstPairTriangle <= 0 ? 1 : 2;
+      this.i = 2 * xCompletePairs + xRemainderTriangles;
+    }
+    this.drawTiling();
+  }
+
   start() {
-    this.draw();
-    window.addEventListener('resize', this.draw.bind(this));
+    const draw = this.drawTiling.bind(this);
+    const onClick = this.onClick.bind(this);
+    window.addEventListener('resize', draw);
+    window.addEventListener('click', onClick);
+    draw();
   }
 }
 
 export class Triangle extends Canvas {
-  constructor(i, j, orientation) {
+  constructor(i, j, orientation, options = {}) {
     super(document.getElementById('background'));
     if (orientation === 'up') {
       this.x =
@@ -65,6 +114,10 @@ export class Triangle extends Canvas {
       this.y = j * EDGE_LENGTH;
       this.yCoefficient = 1;
     }
+    this.options = {
+      ...TRIANGLE,
+      ...options,
+    };
     this.i = i;
     this.j = j;
   }
@@ -81,7 +134,8 @@ export class Triangle extends Canvas {
     ctx.lineTo(this.x, this.y);
     ctx.closePath();
     ctx.lineWidth = 1;
-    ctx.strokeStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+    // ctx.strokeStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+    ctx.strokeStyle = this.options.strokeStyle;
     ctx.stroke();
 
     // Padding
@@ -91,9 +145,10 @@ export class Triangle extends Canvas {
       this.j >= this.jMax - CANVAS.paddingBottom ||
       this.i >= this.iMax - CANVAS.paddingRight
     ) {
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = 'white';
     } else {
-      ctx.fillStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+      // ctx.fillStyle = COLORS[Math.floor(Math.random() * COLORS.length)];
+      ctx.fillStyle = this.options.fillStyle;
     }
     ctx.fill();
   }
